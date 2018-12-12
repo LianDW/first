64a371adbad8a285ec4f22e744b45dce94d0a7ef
xugang <xugang@Lian-workPC.mshome.net>
2018-12-12 17:54:57 +0800

    first commit

---
+++ src/com/lian/test/Test.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2018 sinosoft  Co. Ltd.
+ * All right reserved.
+ */
+package com.lian.test;
+
+import java.rmi.RemoteException;
+
+
+public class Test {
+	public static void main(String[] args) {
+		//fadsfasf
+		System.out.println(12324323);
+		System.out.println(12324323);
+	}
+	
+}
